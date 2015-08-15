1c639812b315e3f58b75b7dbadb36e4a3e4ea723
batesjernigan <bates1012@aol.com>
2015-08-15 11:02:23 -0400

    Add account activation, end of ch 10.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
-  before_save { self.email = self.email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_create :create_activation_digest
+  before_save :downcase_email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   INVALID_PASSWORD = /password/i
 
@@ -31,8 +32,28 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def activate
+    update_attributes(activated: true, activated_at: Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+
+  def downcase_email
+    self.email = self.email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,13 +10,32 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
   end
 
-  test 'valid signup information' do
-    get signup_path
+  test 'valid signup information with account activation' do
+    get(signup_path)
     assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: { name: 'example user', email: "user@example.com",
-        password: "samplePhrase", password_confirmation: "samplePhrase" }
+      post(users_path, user: { name: 'example user', email: "user@example.com",
+        password: "samplePhrase", password_confirmation: "samplePhrase" })
     end
-    assert_template 'users/show'
-    assert is_logged_in?
+    assert_equal(1, ActionMailer::Base.deliveries.size)
+    user = assigns(:user)
+    assert_not(user.activated?)
+    
+    # Try to Log in before activation
+    log_in_as(user)
+    assert_not(is_logged_in?)
+    
+    # Invalid Activation Token
+    get(edit_account_activation_path('invalid token'))
+    assert_not(is_logged_in?)
+    # Vaid Activation Token, invalid email
+    get(edit_account_activation_path(user.activation_token, email: 'wrong'))
+    assert_not(is_logged_in?)
+
+    # Valid activation token and email
+    get(edit_account_activation_path(user.activation_token, email: user.email))
+    assert(user.reload.activated?)
+    follow_redirect!
+    assert_template('users/show')
+    assert(is_logged_in?)
   end
 end
